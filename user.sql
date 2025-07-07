2f9ae6816b34502f263d18b16b704317f2a89a10
ImpulZzZ <lukas.drothler@web.de>
2025-07-07 17:27:42 +0200

    Create user table with necessary fields and constraints

---
+++ user.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` varchar(36) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `email_verified` tinyint(1) NOT NULL DEFAULT '0',
+  `premium_level` int DEFAULT '0',
+  `stripe_customer_id` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_seen` timestamp NULL DEFAULT NULL,
+  `disabled` tinyint(1) NOT NULL DEFAULT '0',
+  `hashed_password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
