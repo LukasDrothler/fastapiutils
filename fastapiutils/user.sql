ef59e7d06173c8e4f18dd5519e6165daacc04983
ImpulZzZ <lukas.drothler@web.de>
2025-07-08 13:08:10 +0200

    Add unique constraints for username and email in user table

--- fastapiutils/user.sql
+++ fastapiutils/user.sql
@@ -12,6 +12,8 @@ CREATE TABLE `user` (
   `last_seen` timestamp NULL DEFAULT NULL,
   `disabled` tinyint(1) NOT NULL DEFAULT '0',
   `hashed_password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
